49e9bc1ab670a43b18d454984ecc7d5de2dac769
unknown <alexisuwimana@gmail.com>
2019-06-01 15:55:30 +0200

    Change titles and styling on homepage4

--- alexis-uwimana/src/App.js
+++ alexis-uwimana/src/App.js
@@ -1,22 +1,19 @@
-import React from 'react';
-import 'bulma/css/bulma.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import {HashRouter, switch, Router } from 'react-router-dom';
-import './App.css';
-import NotFound from './components/NotFound/NotFound';
-
+import React from "react";
+import "bulma/css/bulma.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import { HashRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
     <div className="app">
       <HashRouter basename="/">
-        <switch>
-          <Router pathc>
-          
-          </Router>
-        </switch>
+        <Switch>
+          <Route path="/" component={NotFound} />
+          <Route path="*" component={NotFound} />
+        </Switch>
       </HashRouter>
-
     </div>
   );
 }
